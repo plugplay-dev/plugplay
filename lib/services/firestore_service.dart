25c258ba78c65a27f35fa5d6e6d0b03645b2e169
Aaron Kumwenda <kzaaron883@gmail.com>
2026-07-14 13:25:48 -0800

    feat: implement Firebase Authentication

---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirestoreService {
+  FirestoreService._();
+
+  static final FirestoreService instance = FirestoreService._();
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  User? get _user => FirebaseAuth.instance.currentUser;
+
+  String get _uid => _user!.uid;
+
+  DocumentReference<Map<String, dynamic>> get userDoc =>
+      _firestore.collection('users').doc(_uid);
+
+  Future<void> createUserDocument() async {
+    await userDoc.set({
+      'email': _user?.email,
+      'createdAt': FieldValue.serverTimestamp(),
+    }, SetOptions(merge: true));
+  }
+}
